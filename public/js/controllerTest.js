bcea376ff9170aeee98ba6cd6dfd1e0b23b4215c
zakkyzebra <zzaacchhyy010@yahoo.com>
2015-09-08 11:01:59 -0500

    basic structure added

---
+++ public/js/controllerTest.js
@@ -0,0 +1,9 @@
+"use strict";
+
+(function() {
+    var app = angular.module("controllerTest", []);
+
+    app.controller("CartController", function() {
+        // Controller code goes in here
+    });
+})();
