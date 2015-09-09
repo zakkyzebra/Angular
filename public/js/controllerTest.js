91baa30c7e1bdf5c993179a83ea736c89cadbc43
zakkyzebra <zzaacchhyy010@yahoo.com>
2015-09-09 09:22:47 -0500

    adding the things i didnt have before

--- public/js/controllerTest.js
+++ public/js/controllerTest.js
@@ -3,40 +3,14 @@
 (function() {
     var app = angular.module("controllerTest", []);
 
-    app.controller("CartController", function($scope) {
+    app.controller("CartController", function(){
+    	this.newItem 	= {};
+    	this.item 		= [];
     	
-        // Item List Arrays
-	    $scope.items = [];
-	    $scope.checked = [];
-
-	    // Add a Item to the list
-	    $scope.addItem = function () {
-
-	        $scope.items.push({
-	            amount: $scope.itemAmount,
-	            name: $scope.itemName
-	        });
-
-	        // Clear input fields after push
-	        $scope.itemAmount = "";
-	        $scope.itemName = "";
-
-	    };
-
-	    // Add Item to Checked List and delete from Unchecked List
-	    $scope.toggleChecked = function (index) {
-	        $scope.checked.push($scope.items[index]);
-	        $scope.items.splice(index, 1);
-	    };
-
-	    // Get Total Items
-	    $scope.getTotalItems = function () {
-	        return $scope.items.length;
-	    };
-
-	    // Get Total Checked Items
-	    $scope.getTotalCheckedItems = function () {
-	        return $scope.checked.length;
-	    };
+    	this.addItem = function (){
+    		this.item.push(this.newItem);
+	    	this.newItem 	= {};
+    	};
     });
+
 })();
--- public/js/modelTest.js
+++ public/js/modelTest.js
@@ -2,4 +2,17 @@
 
 (function() {
     var app = angular.module("modelTest", []);
+
+
+    app.controller("UsersController", function(){
+    	this.newUser 	= {};
+    	this.users 		= [];
+    	
+    	this.addUser = function (){
+    		this.users.push(this.newUser);
+	    	this.newUser 	= {};
+    	};
+    });
+
+
 })();
